c370ad67bc7b34482583f77791682ae6f54d4d35
Heber Scachetti <heber.augusto@gmail.com>
2021-02-03 17:24:32 +0000

    updating notebook and train script
    [i] changed the metrics to auc_weighted which is more suitable for fraud detection
    [i] changed model from train script to GradienteBoosting

--- train.py
+++ train.py
@@ -1,11 +1,10 @@
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
 import argparse
 import os
 import numpy as np
-from sklearn.metrics import mean_squared_error
 import joblib
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
@@ -19,10 +18,10 @@ def clean_data(data):
     return x_df, y_df
 
 ws = Workspace.from_config()
-label = "Class"
-dataset = ws.datasets[label]
+key = "creditcard"
+dataset = ws.datasets[key]
 
-x, y = clean_data(ds)
+x, y = clean_data(dataset)
 
 # Split data into train and test sets.
 x_train, x_test, y_train, y_test = train_test_split(x, y , test_size=0.33, random_state=42)
@@ -36,20 +35,24 @@ def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
-    parser.add_argument('--solver', type=str, default='lbfgs', help="Algorithm to use in the optimization problem")
+    parser.add_argument('--n_estimators', type=int, default=100, help="The number of estimators")
+    parser.add_argument('--learning_rate', type=float, default=0.1, help="Learning rate")
+    parser.add_argument('--max_depth', type=int, default=3, help="The maximum depth of each estimators")
 
     args = parser.parse_args()
 
-    run.log("Regularization Strength:", np.float(args.C))
-    run.log("Max iterations:", np.int(args.max_iter))
-    run.log("Solver:", np.str(args.solver))    
+    run.log("n_estimators:", np.int(args.n_estimators))
+    run.log("learning_rate:", np.float(args.learning_rate))
+    run.log("max_depth:", np.int(args.max_depth))    
 
-    model = LogisticRegression(C=args.C, max_iter=args.max_iter, solver=args.solver).fit(x_train, y_train)
+    model = GradientBoostingClassifier(
+        n_estimators=args.n_estimators, 
+        learning_rate=args.learning_rate, 
+        max_depth=args.max_depth).fit(x_train, y_train)
 
-    accuracy = model.score(x_test, y_test)
-    run.log("Accuracy", np.float(accuracy))
+    auc = roc_auc_score(y_test, model.predict_proba(x_test)[:,1], average='weighted')
+   
+    run.log("AUC_weighted", np.float(auc))
 
 if __name__ == '__main__':
     main()
