1f7f54cf7636b73347dd9c6af64fd29ef37d3988
Heber Scachetti <heber.augusto@gmail.com>
2021-02-04 05:49:15 +0000

    model   deploy

--- score.py
+++ score.py
@@ -1,6 +1,7 @@
 import pickle
 import json
 import pandas as pd
+import  numpy as  np
 from sklearn.externals import joblib
 from azureml.core.model import Model
 
@@ -10,9 +11,11 @@ def init():
     model_path = Model.get_model_path('fraud_detection_model')
     model = joblib.load(model_path)
 
-def run(raw_data):
-    data = np.array(json.loads(raw_data)['data'])
-    # make prediction
-    y_hat = model.predict(data)
-    # you can return any data type as long as it is JSON-serializable
-    return y_hat.tolist()
+def run(data):
+    try:
+        data = pd.DataFrame(json.loads(data)['data'])
+        result = model.predict(data)
+        return result.tolist()
+    except Exception as e:
+        error = str(e)
+        return error
